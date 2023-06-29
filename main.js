84bc056ec97c055250773fac5889ef5762eb1734
Piero Alexander Huayta Pimentel <pieroalexander555@gmail.com>
2023-06-29 13:20:40 -0500

    Articles

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const low = require("lowdb");
+const articleRoutes = require("./routes/articles");
 
 //Set Port
-const port = process.env.PORT || 8087;
+const port = process.env.PORT || 8080;
 
+//GetDb File
 const FileSync = require("lowdb/adapters/FileSync");
 
 const adapter = new FileSync("db.json");
@@ -18,9 +20,31 @@ const app = express();
 
 app.db = db;
 
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Articles API - CERTUS",
+            version: "0.0.1",
+            descrption: "Demo API for sales"
+        },
+        servers: [
+            {
+                url: "http://localhost:" + port,
+            }
+        ]
+    },
+    apis: [
+        "./routes/*.js"
+    ]
+}
+
+
+
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use("/api/articles", articleRoutes);
 
 app.listen(port, () => console.log("App is running!!!! on port", port));
 
